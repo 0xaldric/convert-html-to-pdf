8c550933179637b6207d84e8781f6552f5e7579e
anhnd350309 <anhnd350309@gmail.com>
2025-01-02 11:18:43 +0700

    feat: upload file html

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class HTMLInput(BaseModel):
 
 # Output directory for PDFs
 PDF_OUTPUT_DIR = "generated_pdfs"
+HTML_OUTPUT_DIR = "generated_htmls"
 # Create directory if it doesn't exist
 os.makedirs(PDF_OUTPUT_DIR, exist_ok=True)
 
@@ -42,15 +43,22 @@ async def generate_pdf(input: HTMLInput):
     """
     try:
         # Generate a unique filename
-        filename = f"{uuid.uuid4()}.pdf"
+        name = str(uuid.uuid4())
+        filename = f"{name}.pdf"
+        html_filename = f"{name}.html"
         output_path = os.path.join(PDF_OUTPUT_DIR, filename)
+        html_output_path = os.path.join(HTML_OUTPUT_DIR, html_filename)
 
         # Convert HTML to PDF
-        data = pdfkit.from_string(
+        pdfkit.from_string(
             input.html, output_path, configuration=PDFKIT_CONFIG)
+
+        with open(html_output_path, 'w') as html_file:
+            html_file.write(input.html)
 
         # Upload PDF to S3
         path = ''
+        html_path = ''
         today = datetime.datetime.now().strftime("%Y-%m-%d")
         prefix = f"{PREFIX}{today}/"
         filename_to_upload = prefix+filename
@@ -62,11 +70,21 @@ async def generate_pdf(input: HTMLInput):
             if not path:
                 raise HTTPException(
                     status_code=500, detail="Failed to upload PDF to S3")
+        html_filename_to_upload = prefix+html_filename
+        with open(html_output_path, 'rb') as html_file:
+            html_path = s3_handler_instance.upload_file_data_to_s3(
+                file_data=html_file,
+                file_name=html_filename_to_upload
+            )
+            if not html_path:
+                raise HTTPException(
+                    status_code=500, detail="Failed to upload HTML to S3")
 
         os.remove(output_path)
+        os.remove(html_output_path)
 
         # Return the filename
-        return {"filename": filename_to_upload, "path": path}
+        return {"filename": filename_to_upload, "path": path, "html_path": html_path}
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Error generating PDF: {str(e)}")
